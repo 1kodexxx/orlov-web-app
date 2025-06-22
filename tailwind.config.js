25e7ff4ac8da43d544c52dd19e57ae98acdeb74a
pvntheraxxx <pvntheraxxx@gmail.com>
2025-06-22 15:46:52 +0300

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
+  darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: { DEFAULT: "#EFE393", contrast: "#1a1a1a" },
+        secondary: "#2C2C2C",
+        background: {
+          DEFAULT: "#181818",
+          paper: "#222222",
+        },
+        text: {
+          primary: "#EFE393",
+          secondary: "#CCCCCC",
+        },
+        corporate: {
+          success: "#A2E3C4",
+          error: "#FFB3B3",
+          info: "#9ECFFF",
+          warning: "#FCE38A",
+          surface: "#3A3A3A",
+        },
+      },
+    },
   },
   plugins: [],
 };
