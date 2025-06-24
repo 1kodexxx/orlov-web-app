58a081cd3c45ccfdb7b8920c0858b320b83ed083
pvntheraxxx <pvntheraxxx@gmail.com>
2025-06-24 16:45:58 +0300

    Group

--- src/components/products/ProductFilterPanel.tsx
+++ src/components/products/ProductFilterPanel.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Search from "@/components/common/Search";
+import Search from "@/components/products/Search";
 import FilterDropdown from "@/components/products/FilterDropdown";
 import PriceFilter from "@/components/products/PriceFilter";
 
