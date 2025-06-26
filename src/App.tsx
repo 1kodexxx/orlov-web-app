f260948747c8e5198bbc983e2a4563c7c45d4bf9
pvntheraxxx <pvntheraxxx@gmail.com>
2025-06-26 16:11:16 +0300

    restructed

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,39 @@
 import { Routes, Route } from "react-router-dom";
-import Layout from "@/layout/Layout";
+import { ScrollToTop } from "@/components/common/";
+import ProductPage from "@/components/shop/productPage/ProductPage";
+import { Footer, NavBar } from "@/components/layout";
 
-import ScrollToTop from "@/components/common/ScrollToTop";
-import ProductPage from "@/components/shop/ProductPage";
-
-import Home from "@/pages/Home";
-import AboutUs from "@/pages/AboutUs";
-import Catalog from "@/pages/Catalog";
-import Contacts from "@/pages/Contacts";
-import Delivery from "@/pages/Delivery";
-import Reviews from "@/pages/Reviews";
-import Cart from "@/pages/Cart";
-import NotFound from "@/pages/NotFound";
+import {
+  Home,
+  Catalog,
+  Cart,
+  AboutUs,
+  Contacts,
+  Delivery,
+  Reviews,
+  NotFound,
+} from "@/pages";
 
 const App = () => {
   return (
-    <Layout>
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about-us" element={<AboutUs />} />
-        <Route path="/catalog" element={<Catalog />} />
-        <Route path="/contacts" element={<Contacts />} />
-        <Route path="/delivery" element={<Delivery />} />
-        <Route path="/reviews" element={<Reviews />} />
-        <Route path="/cart" element={<Cart />} />
-
-        <Route path="/catalog/:id" element={<ProductPage />} />
-
-        {/* Обработка несуществующих маршрутов */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Layout>
+    <div className="min-h-screen bg-background text-text-primary font-sans flex flex-col">
+      <NavBar />
+      <main className="flex-1">
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/delivery" element={<Delivery />} />
+          <Route path="/reviews" element={<Reviews />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/catalog/:id" element={<ProductPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 };
 
---
+++ src/pages/index.ts
@@ -0,0 +1,8 @@
+export { default as AboutUs } from "./AboutUs";
+export { default as Cart } from "./Cart";
+export { default as Catalog } from "./Catalog";
+export { default as Contacts } from "./Contacts";
+export { default as Delivery } from "./Delivery";
+export { default as Home } from "./Home";
+export { default as NotFound } from "./NotFound";
+export { default as Reviews } from "./Reviews";
