ce9164fc291c0224c1c9e3dea332ca11803b13b1
pvntheraxxx <pvntheraxxx@gmail.com>
2025-06-27 20:48:08 +0300

    Deleted ratings ProductDetails.tsx

--- src/components/shop/productPage/ProductDetails.tsx
+++ src/components/shop/productPage/ProductDetails.tsx
@@ -91,39 +91,6 @@ export default function ProductDetails({
         </h1>
       </div>
 
-      {/* Блок рейтинга */}
-      <div
-        className={`flex flex-col sm:flex-row sm:items-center mb-4 gap-4 transition-all duration-700 ease-out delay-100 ${
-          isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
-        }`}>
-        <span className="flex items-center">
-          {[...Array(4)].map((_, i) => (
-            <svg
-              key={i}
-              fill="currentColor"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              viewBox="0 0 24 24"
-              className="w-4 h-4 text-gold">
-              <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" />
-            </svg>
-          ))}
-          <svg
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            viewBox="0 0 24 24"
-            className="w-4 h-4 text-gold">
-            <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" />
-          </svg>
-          <span className="ml-2 text-text-secondary">0 отзывов</span>
-        </span>
-      </div>
-
       {/* Блок описания */}
       <p
         className={`leading-relaxed text-text-secondary transition-all duration-700 ease-out delay-200 ${
